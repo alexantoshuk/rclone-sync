4508cdb25b59b312a7fbbe2fec1675a34341efb5
Ali <alexander.antoshuk@gmail.com>
2023-02-20 17:05:37 +0300

    fix: invalid extention case

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     scripts=['scripts/rclone-sync.py'],
     license='Apache Software License',
     description='Monitor changes in selected local folder and sync (one-way) to selected cloud storage',
-    long_description=open('README.MD').read(),
+    long_description=open('README.md').read(),
     install_requires=['python-rclone', 'watchdog'],
     url='https://github.com/alexantoshuk/rclone-sync',
     author='Alexander Antoshuk',
