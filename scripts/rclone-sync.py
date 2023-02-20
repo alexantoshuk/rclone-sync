a7379388c318709eaf2d5876e7abf6209c7f3b1d
Ali <alexander.antoshuk@gmail.com>
2023-02-20 16:59:41 +0300

    feat: first commit

---
+++ scripts/rclone-sync.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import rclone
+import watchdog
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='rclone-sync',
+    version='0.1.0',
+    scripts=['scripts/rclone-sync.py'],
+    license='Apache Software License',
+    description='Monitor changes in selected local folder and sync (one-way) to selected cloud storage',
+    long_description=open('README.MD').read(),
+    install_requires=['python-rclone', 'watchdog'],
+    url='https://github.com/alexantoshuk/rclone-sync',
+    author='Alexander Antoshuk',
+    author_email='alexander.antoshuk@gmail.com',
+)
